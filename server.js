c939b7679bbe8b53e0a231ae5035d5a57d265656
batrissya <104118611+namileeya@users.noreply.github.com>
2026-01-06 21:13:22 +0800

    change port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Serving Vite app on http://0.0.0.0:${PORT}`);
 });
