104adc63d78d4d8e4bdfa89972be3fbe75a97562
batrissya <104118611+namileeya@users.noreply.github.com>
2026-01-06 21:39:27 +0800

    allowed host = true

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['bloodconnectwebnv-1.onrender.com'],
+    allowedHosts: true,
     host: '0.0.0.0',
     port: 10000,
     open: true
